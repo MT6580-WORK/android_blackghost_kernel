793878bca6619fc65825eed25340c0a59464cfab
melekpro <melekpro6@gmail.com>
2024-02-23 16:07:15 +0100

    Update mt65xx_lcm_list.h

--- drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
+++ drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
@@ -23,6 +23,7 @@ extern struct LCM_DRIVER hct_otm1282_dsi_vdo_hd_auo;
 extern struct LCM_DRIVER hct_rm68200_dsi_vdo_hd_cpt;
 extern struct LCM_DRIVER hct_nt35521s_dsi_vdo_hd_boe_50_xld;
 extern struct LCM_DRIVER hct_hx8394d_dsi_vdo_hd_cmi;
+extern struct LCM_DRIVER gc9503p_fwp_dsi_vdo_jt_ivo_ba2;
 
 #ifdef BUILD_LK
 extern void mdelay(unsigned long msec);
